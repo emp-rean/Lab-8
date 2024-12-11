ec7e516a1eab0c3a516269bfcb1261f3dbe607bb
emp-rean <spforimport@gmail.com>
2024-12-11 14:21:23 +0200

    54

---
+++ Lab-8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void prog1()
+{
+    
+}
+
+/*void prog2()
+{
+    
+}
+*/
+
+/*void prog3()
+{
+    
+}
+*/
+
+int main()
+{
+prog1();
+//prog2();
+//prog3();
+}
